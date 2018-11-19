6b3cd132bf89547a3a99b445c3d8f96db0e82265
Ashen <miss525vip@163.com>
2018-11-19 22:16:24 +0800

    添加版本控制

---
+++ git-test-project/src/com/Ashen/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.Ashen.git;
+
+/**
+ * Created by Ashen on 2018/11/19.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git come here!");
+        System.out.println("git come here!!");
+        System.out.println("git come here!!!");
+
+    }
+}
